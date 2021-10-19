8078da3e332f16fda963eba00fbffd9f36f46a1c
CarolHeChuanxiuyue <49880578+CarolHeChuanxiuyue@users.noreply.github.com>
2021-10-19 14:49:42 -0700

    first commit

---
+++ Assets/Scripts/DragandDrop.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragandDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler {
+	private RectTransform rectTransform;
+	public GameObject image;
+	public void Start () {
+		image.SetActive(false);
+	}
+	
+	public void OnPointerEnter(PointerEventData eventData){
+		image.SetActive(true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		image.SetActive(false);
+	}
+	private void Awake(){
+		rectTransform = GetComponent<RectTransform>();
+	}
+	public void OnPointerDown(PointerEventData eventData){
+		Debug.Log("OnPointerDown");
+		image.SetActive(true);
+	}
+	public void OnDrag(PointerEventData eventData){
+		Debug.Log("OnDrag");
+		image.SetActive(true);
+		rectTransform.anchoredPosition += eventData.delta;
+	}
+	public void OnBeginDrag(PointerEventData eventData){
+		Debug.Log("BeginDrag");
+		image.SetActive(true);
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		Debug.Log("EndDrag");
+	}
+
+}
---
+++ Assets/Scripts/FinishedClick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class FinishedClick : MonoBehaviour {
+	[SerializeField] GameObject[] Objects;
+    public InputField idInput;
+
+	public void RecordLocations(){
+    	var csv = new StringBuilder();
+        var columnNames = "ParticipantID,ImageName,xPos,yPos";
+        string[] objNames;
+        objNames = new string[12]{ "Wheelbarrow", "Well", "Trashcan", "Telescope", "Plant", "Picnic Table", "Piano", "Mailbox", "Harp", "Chair", "Bookshelf", "Stove" };
+        csv.AppendLine(columnNames);
+        for (int i = 0; i < Objects.Length; i++)
+        {
+            var xPos = Objects[i].transform.position.x.ToString();
+            var yPos = Objects[i].transform.position.y.ToString();
+            var newLine = string.Format("{0},{1},{2},{3}", idInput.text, objNames[i], xPos, yPos);
+            csv.AppendLine(newLine);
+        }
+        string filePath = @"UserData\"+idInput.text.ToString()+".csv";
+        //after your loop
+        File.WriteAllText(filePath, csv.ToString());
+        // move to next scene
+        SceneManager.LoadScene(1);
+    }
+}
